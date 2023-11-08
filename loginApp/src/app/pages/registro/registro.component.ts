fb253f48d94af962119947c557d801346c9ce0d6
Fridanahuat <114037872+Fridanahuat@users.noreply.github.com>
2023-11-07 21:21:04 -0500

    inicio del proyecto
    commi1

---
+++ loginApp/src/app/pages/registro/registro.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+usuario:UsuarioModel= new UsuarioModel();
+  constructor() { }
+
+  ngOnInit() {
+    this.usuario= new UsuarioModel();
+    this.usuario.email='fridan@gmail.com';
+  }
+
+
+}
