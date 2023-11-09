d3face2453e32f7651193dbdb276ee616f9674ec
Fridanahuat <114037872+Fridanahuat@users.noreply.github.com>
2023-11-08 19:10:27 -0500

    mejora la validacion del token
    video 18

--- loginApp/src/app/pages/home/home.component.ts
+++ loginApp/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +10,16 @@ import { AuthService } from '../../services/auth.service';
 export class HomeComponent implements OnInit {
 
   constructor( private auth: AuthService,
-                private router: Router) { }
+                private router: Router ) { }
 
   ngOnInit() {
   }
 
-  salir(){
+  salir() {
+
     this.auth.logout();
     this.router.navigateByUrl('/login');
+
   }
+
 }
